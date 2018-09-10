82e9df360446d47d6793b4515f0630972078fedd
Vitalii Balashov <Vitalii_Balashov@epam.com>
2018-09-10 18:45:41 +0300

    initioal commit

---
+++ wordfence-json.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Plugin Name: Wordfence JSON
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Send Wordfence notifications in JSON to listener of your vulnerability management system
+Version: 1.0
+Author: Vitalii_Balashov
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
+
+$rp_endpoint = 'set_the_endpoint'; // endpoint of report portal
+
+
+
+//global $wpdb;
+//$all_issues = $wpdb->get_results('SELECT * FROM wp_wfIssues');
+
+//var_dump(json_encode($all_issues));
