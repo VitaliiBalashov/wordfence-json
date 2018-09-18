f4474b8ab1694b56caba61adaf581ca61f6daf4f
Vitalii Balashov <vitalii.balashov@dowjones.com>
2018-09-19 00:15:44 +0300

    first working version

--- wordfence-json.php
+++ wordfence-json.php
@@ -10,11 +10,62 @@ Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
 */
 
-$rp_endpoint = 'set_the_endpoint'; // endpoint of report portal
+$table_name = 'wp_wfissues';
+
+function send_new_issues($issues, $endpoint) {
+    $args = array (
+        'timeout' => 45,
+        'blocking' => false,
+        'redirection' => 5,
+        'httpversion' => '1.0',
+        'headers' => array('Content-Type' => 'application/json; charset=utf-8'),
+        'body' => wp_json_encode($issues),
+        'method' => 'POST'
+    );
+    $response = wp_remote_post($endpoint, $args);
+
+    return $response;
+}
+
+function get_issues() {
+    global $wpdb;
+    $all_issues = $wpdb->get_results('SELECT time, type, severity, shortMsg, LongMsg FROM wp_wfissues');
+
+    // TODO: table name to variable
+    // TODO: get only new issues
+    return $all_issues;
+}
+
+function wordfence_json_check() {
+
+    // Basic config
+
+    $report_portal = 'http://10.207.33.94/monitoring/channel/32701/'; // debug
+
+    $issues = get_issues();
+    $response = send_new_issues($issues, $report_portal);
+
+    return $response;
+}
 
 
+// debug purpouses, set to daily get value from user
 
-//global $wpdb;
-//$all_issues = $wpdb->get_results('SELECT * FROM wp_wfIssues');
+add_filter( 'cron_schedules', 'cron_add_one_min' );
+function cron_add_one_min( $schedules ) {
+    $schedules['one_min'] = array(
+        'interval' => 60,
+        'display' => 'every minute'
+    );
+    return $schedules;
+}
 
-//var_dump(json_encode($all_issues));
+add_action( 'wordfence_json_job', 'wordfence_json_check' );
+
+if (!wp_next_scheduled('wordfence_json_job')) {
+    $scheduled_event = wp_schedule_event(time(), 'one_min', 'wordfence_json_job');
+}
+
+//var_dump(_get_cron_array());
+
+
