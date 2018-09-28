7e84a32348b153bc88b0f8c147ccb4ec2a367b05
Vitalii Balashov <vitaliibBalashov@dowjones.com>
2018-09-28 16:04:08 +0300

    alpha 1.1

--- wordfence-json.php
+++ wordfence-json.php
@@ -16,8 +16,13 @@ $wordfence_json_version = "1.0";
 
 
 $options = array(
-	'wf_table_name' => $wpdb->prefix.'wfIssues',
+	'wf_table_name' => $wpdb->prefix.'wfissues',
 	'wfJson_table_name' => $wpdb->prefix.'wfJson',
+	'project_name' => 'wordpress',
+	'rp_host' => 'https://secautomation.build.pib.dowjones.io',
+	'launch' => 'regular_scan',
+	//'endpoint' => 'http://10.207.33.94/monitoring/channel/32701/'
+	'endpoint' => 'https://secautomation.build.pib.dowjones.io/jiraservice/report'
 );
 
 function wordfence_json_install() {
@@ -63,13 +68,29 @@ function send_new_issues($issues, $endpoint) {
         'blocking' => false,
         'redirection' => 5,
         'httpversion' => '1.0',
+        'sslverify' => false,
         'headers' => array('Content-Type' => 'application/json; charset=utf-8'),
         'body' => wp_json_encode($issues),
         'method' => 'POST'
     );
+    echo "<pre>";
+    var_dump($args);
+    echo "</pre>";
     $response = wp_remote_post($endpoint, $args);
 
     return $response;
+}
+
+function prepare_json($issues) {
+	global $options;
+	$wrapp_array = array(
+		'prj' => $options['project_name'],
+		'host' => $options['rp_host'],
+		'launch' => $options['launch'],
+		'report' => $issues
+	);
+
+	return $wrapp_array;
 }
 
 function get_issues() {
@@ -80,36 +101,37 @@ function get_issues() {
 
 	global $wpdb;
 	global $options;
-	$last_check_point = $wpdb->get_var("SELECT 'lastCheck' FROM $options[wfJson_table_name]");
+	$last_check_point = $wpdb->get_var("SELECT lastCheck FROM $options[wfJson_table_name]");
+	var_dump($last_check_point);
 
 	// Fixing current timestamp to next use.
 	// It prevents different timestamps in different places.
 
 	$current_time = time();
 
-
-	$wpdb->update( $options['wfJson_table_name'], array('lastCheck' => $current_time), array('ID'=>1));
+	//$wpdb->update( $options['wfJson_table_name'], array('lastCheck' => $current_time), array('ID'=>1));
 
     $all_issues = $wpdb->get_results(
     	"SELECT
- 					'time',
- 					'lastUpdated', 
- 					'type', 
- 					'severity', 
- 					'shortMsg', 
- 					'LongMsg' 
+ 					time,
+ 					lastUpdated, 
+ 					type, 
+ 					severity, 
+ 					shortMsg, 
+ 					longMsg 
  				FROM 
  					$options[wf_table_name] 
  				WHERE 
- 					'status' = 'new' 
+ 					status = 'new' 
  					AND 
- 					'lastUpdated' 
+ 					lastUpdated 
  					BETWEEN $last_check_point AND $current_time"
 				);
 
     // Prevent sending empty array
 
     if (!empty($all_issues)) {
+    	$all_issues = prepare_json($all_issues);
     	return $all_issues;
     }
     else {
@@ -126,11 +148,10 @@ function wordfence_json_check() {
     // Define the endpoint.
 	// The endpoint is the receiver of sending JSONs
 
-
-    $report_portal = 'http://10.207.33.94/monitoring/channel/32701/'; // debug
+	global $options;
 
     if ($issues = get_issues()) {
-	    $response = send_new_issues($issues, $report_portal);
+	    $response = send_new_issues($issues, $options['endpoint']);
 	    return $response;
     }
      /* ToDo: should the function return something in case of error?
@@ -139,31 +160,31 @@ function wordfence_json_check() {
 
 }
 
+wordfence_json_check();
 
-
-/* debug purpouses, set to daily get value from user
- * ToDo: this filter should be removed when debugging will be end.
- * Plugin can check the new issues daily or twice per day.
- * To define it we should know, when WP admin panel using by admins/users usually.
- */
-
-
-add_filter( 'cron_schedules', 'cron_add_one_min' );
-function cron_add_one_min( $schedules ) {
-    $schedules['one_min'] = array(
-        'interval' => 60,
-        'display' => 'every minute'
-    );
-    return $schedules;
-}
-
-
-// Creating the job for WP-Cron.
-// Pay attention, that this job will run only when admin panel is open
-// ToDo: change it to daily or twice per day
-
-add_action( 'wordfence_json_job', 'wordfence_json_check' );
-
-if (!wp_next_scheduled('wordfence_json_job')) {
-    $scheduled_event = wp_schedule_event(time(), 'one_min', 'wordfence_json_job');
-}
+///* debug purpouses, set to daily get value from user
+// * ToDo: this filter should be removed when debugging will be end.
+// * Plugin can check the new issues daily or twice per day.
+// * To define it we should know, when WP admin panel using by admins/users usually.
+// */
+//
+//
+//add_filter( 'cron_schedules', 'cron_add_one_min' );
+//function cron_add_one_min( $schedules ) {
+//    $schedules['one_min'] = array(
+//        'interval' => 60,
+//        'display' => 'every minute'
+//    );
+//    return $schedules;
+//}
+//
+//
+//// Creating the job for WP-Cron.
+//// Pay attention, that this job will run only when admin panel is open
+//// ToDo: change it to daily or twice per day
+//
+//add_action( 'wordfence_json_job', 'wordfence_json_check' );
+//
+//if (!wp_next_scheduled('wordfence_json_job')) {
+//    $scheduled_event = wp_schedule_event(time(), 'one_min', 'wordfence_json_job');
+//}
